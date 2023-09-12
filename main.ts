1a4b45744596d5dba6b66edeef6a167e8740fcee
kyle-lyver <144264052+kyle-lyver@users.noreply.github.com>
2023-09-12 16:01:43 -0400

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,8 @@
-/* Copyright (c) 2020 MTHS All rights reserved
+/* Copyright (c) 2023 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Kyle Lyver
+ * Created on: Sep 2023
+ * This program displays 'Hello, Kyle'
 */
 
-basic.showString('Hello, World!')
+basic.showString('Hello, Kyle!')
